1df5b70342ec36857188b9a9c51e5343bd5e5a78
UttamPatel0164 <uttamcse1@gmail.com>
2025-08-11 12:29:01 +0530

    prefix good quation

---
+++ Day57.cpp
@@ -0,0 +1,85 @@
+2438. Range Product Queries of Powers
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given a positive integer n, there exists a 0-indexed array called powers, composed of the minimum number of powers of 2 that sum to n. The array is sorted in non-decreasing order, and there is only one way to form the array.
+
+You are also given a 0-indexed 2D integer array queries, where queries[i] = [lefti, righti]. Each queries[i] represents a query where you have to find the product of all powers[j] with lefti <= j <= righti.
+
+Return an array answers, equal in length to queries, where answers[i] is the answer to the ith query. Since the answer to the ith query may be too large, each answers[i] should be returned modulo 109 + 7.
+
+ 
+
+Example 1:
+
+Input: n = 15, queries = [[0,1],[2,2],[0,3]]
+Output: [2,4,64]
+Explanation:
+For n = 15, powers = [1,2,4,8]. It can be shown that powers cannot be a smaller size.
+Answer to 1st query: powers[0] * powers[1] = 1 * 2 = 2.
+Answer to 2nd query: powers[2] = 4.
+Answer to 3rd query: powers[0] * powers[1] * powers[2] * powers[3] = 1 * 2 * 4 * 8 = 64.
+Each answer modulo 109 + 7 yields the same answer, so [2,4,64] is returned.
+Example 2:
+
+Input: n = 2, queries = [[0,0]]
+Output: [2]
+Explanation:
+For n = 2, powers = [2].
+The answer to the only query is powers[0] = 2. The answer modulo 109 + 7 is the same, so [2] is returned.
+ 
+
+Constraints:
+
+1 <= n <= 109
+1 <= queries.length <= 105
+0 <= starti <= endi < powers.length
+class Solution {
+public:
+    int mod = 1e9 + 7;
+
+    long long modPow(long long base, long long exp) {
+        long long res = 1;
+        while (exp > 0) {
+            if (exp & 1) res = (res * base) % mod;
+            base = (base * base) % mod;
+            exp >>= 1;
+        }
+        return res;
+    }
+
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+        vector<int> pow;
+        while (n > 0) {
+            for (int i = 30; i >= 0; i--) {
+                int x = 1 << i;
+                if (n >= x) {
+                    pow.push_back(i); // store exponent
+                    n -= x;
+                }
+            }
+        }
+        reverse(pow.begin(), pow.end());
+
+        vector<int> prefix(pow.size());
+        prefix[0] = pow[0];
+        for (int i = 1; i < pow.size(); i++) {
+            prefix[i] = pow[i] + prefix[i - 1];
+        }
+
+        vector<int> ans;
+        for (auto &q : queries) {
+            int expSum;
+            if (q[0] == 0) {
+                expSum = prefix[q[1]];
+            } else {
+                expSum = prefix[q[1]] - prefix[q[0] - 1];
+            }
+            ans.push_back((1<<expSum)%mod);
+        }
+        return ans;
+    }
+};
