1f467b82e9dbb88893c931d26e7bfb244d817cfd
UttamPatel0164 <uttamcse1@gmail.com>
2025-09-16 20:16:43 +0530

    stack

---
+++ Day83.java
@@ -0,0 +1,14 @@
+public class Day83 
+{
+    public static void main(String[] args) 
+    {
+        String firstName = "John";
+        String lastName = "Doe";
+
+        // Concatenation with +
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName); // Output: John Doe
+    }
+
+    
+}
